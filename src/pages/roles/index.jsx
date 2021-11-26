990b550d4935dfd99ab336db7c52bd0177aa298f
Abbas Hosseini <hosseini44444@gmail.com>
2021-11-26 15:20:05 +0330

    add Role routes

--- src/pages/roles/index.jsx
+++ src/pages/roles/index.jsx
@@ -1,3 +1,15 @@
-const Page = () => <>Roles</>;
+import { Routes, Route } from "react-router-dom";
 
-export default Page;
+import { RoleList, RoleInfo, RoleAddEdit, NotFound } from "..";
+
+const Roles = () => (
+  <Routes>
+    <Route index element={<RoleList />} />
+    <Route path="details/:id" element={<RoleInfo />} />
+    <Route path="add/:id" element={<RoleAddEdit />} />
+    <Route path="add" element={<RoleAddEdit />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+);
+
+export default Roles;
