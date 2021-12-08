2b23576532a5cd59022f219c02e656b237dc2514
Abbas Hosseini <hosseini44444@gmail.com>
2021-12-08 14:42:30 +0530

    memorize callback

--- src/hooks/useMuiRegister.js
+++ src/hooks/useMuiRegister.js
@@ -1,9 +1,14 @@
+import { useCallback } from "react";
+
 const useMuiRegister = (register) => {
-  const MuiRegister = (name, options, refName = "inputRef") => {
-    const { ref, ...registerMethods } = register(name, options);
-    const updatedMethods = { [refName]: ref, ...registerMethods };
-    return updatedMethods;
-  };
+  const MuiRegister = useCallback(
+    (name, options, refName = "inputRef") => {
+      const { ref, ...registerMethods } = register(name, options);
+      const updatedMethods = { [refName]: ref, ...registerMethods };
+      return updatedMethods;
+    },
+    [register],
+  );
   return MuiRegister;
 };
 
