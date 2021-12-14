fe04f241dbe0a0ed1caf3486f93156363fffbcc1
Abbas Hosseini <hosseini44444@gmail.com>
2021-12-14 20:03:14 +0530

    add user routes

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import UserList from "./list";
+import { UserList, UserAddEdit, UserInfo, NotFound } from "..";
 
 const Users = () => (
   <Routes>
     <Route index element={<UserList />} />
+    <Route path="details/:id" element={<UserInfo />} />
+    <Route path="add/:id" element={<UserAddEdit />} />
+    <Route path="add" element={<UserAddEdit />} />
+    <Route path="*" element={<NotFound />} />
   </Routes>
 );
 
