3c3de2a7879c4e950da2bfae93c6fc7a97b56f9a
Abbas Hosseini <hosseini44444@gmail.com>
2021-12-17 20:41:57 +0530

    set withCredentials to always true

--- src/utils/axios-instance.js
+++ src/utils/axios-instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  withCredentials: process.env.NODE_ENV === "production" ? true : false,
+  withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use(
