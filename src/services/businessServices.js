ab09ab9df50a9873ae3d6430e16bc040a75237dc
Akash Kumar <mailakash3103@gmail.com>
2022-02-09 16:24:16 +0530

    api integration for upload

--- src/services/businessServices.js
+++ src/services/businessServices.js
@@ -1,9 +1,20 @@
 import axios from "../utils/axios-instance";
 
-export async function updateFinance(id, payload) {
+export async function updateFinance(
+  id,
+  bankDetails,
+  paymentChannels,
+  paymentMethods,
+) {
   try {
     const api = `businesses/finances/${id}`;
-    const response = await axios.put(api, payload);
+    console.log(id, bankDetails, paymentChannels, paymentMethods);
+    const response = await axios.put(
+      api,
+      bankDetails,
+      paymentChannels,
+      paymentMethods,
+    );
     console.log(response);
     return response;
   } catch (error) {
